900f184a1e5644ca5a7e7128acd3a1912efc1bca
s27502 <s27502@pjwstk.edu.pl>
2024-03-23 11:38:46 +0100

    Did everything I guess

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Text;
 
 namespace APBD
@@ -8,6 +10,64 @@ namespace APBD
     {
         public static void Main(string[] args)
         {
+            Container.fillDic();
+            // Create containers
+            Container container1 = new Container(5, 10, 100, 3, 10);
+            Console.WriteLine(container1.ToString());
+            Container container2 = new Container(5, 1000, 100, 3, 10);
+            Console.WriteLine(container2.ToString());
+            LiquidContainer liquidContainer1 = new LiquidContainer(6, 12, 150, 4, false, 10);
+            RefrigeratedContainer refrigeratedContainer1 = new RefrigeratedContainer(7, 15, 200, 5, "Apples", 4.4, 10);
+
+            // Load cargo into containers
+            container1.LoadMass(50);
+            liquidContainer1.LoadMass(75);
+            refrigeratedContainer1.LoadMass(100);
+
+            // Create a ship
+            Ship ship1 = new Ship(10, 20, 5000);
+
+            // Load containers onto the ship
+            ship1.LoadContainer(container1);
+            ship1.LoadContainer(liquidContainer1);
+            ship1.LoadContainer(refrigeratedContainer1);
+
+            // Print information about the ship and its cargo
+            Console.WriteLine("Ship Information:");
+            Console.WriteLine(ship1);
+
+            // Unload a container from the ship
+            ship1.RemoveContainer(container1);
+
+            // Print updated information about the ship and its cargo
+            Console.WriteLine("Ship Information After Unloading a Container:");
+            Console.WriteLine(ship1);
+
+            // Replace a container on the ship with another container
+            Container newContainer = new Container(6, 11, 120, 3, 10);
+            Console.WriteLine(newContainer.ToString());
+            ship1.Switch(liquidContainer1.sNumber, newContainer);
+
+            // Print updated information about the ship and its cargo
+            Console.WriteLine("Ship Information After Switching Containers:");
+            Console.WriteLine(ship1);
+
+            // Create another ship
+            Ship ship2 = new Ship(15, 25, 750);
+
+            // Move a container from one ship to another
+            ship1.Switch(liquidContainer1.sNumber, newContainer);
+
+            // Print updated information about both ships and their cargo
+            Console.WriteLine("Information About Both Ships:");
+            Console.WriteLine("Ship 1:");
+            Console.WriteLine(ship1);
+            Console.WriteLine("Ship 2:");
+            Console.WriteLine(ship2);
+
+            // Print information about a specific container
+            Console.WriteLine("Information About a Specific Container:");
+            Console.WriteLine(container1);
         }
     }
 
@@ -17,26 +77,25 @@ namespace APBD
         protected double Weight;
         protected double Height;
         private double Depth;
-        private Dictionary<string, int> Names = new Dictionary<string, int>();
-        private HashSet<string> b = new HashSet<string>();
+        protected static Dictionary<string, int> Names = new Dictionary<string, int>();
         public string sNumber { get; set; }
         protected double MaxMass;
         private static List<Container> cons = new List<Container>();
         
-        public Container(double height, double weight, double maxMass, double depth)
+        public Container(double height, double weight, double maxMass, double depth, double mass)
         {
-            fillDic();
+            Mass = mass;
             Height = height;
-            MaxMass = weight;
             sNumber = $"KON-K-{Names["Container"]}";
             Names["Container"]++;
             MaxMass = maxMass;
             Depth = depth;
+            Weight = weight;
 
             cons.Add(this);
         }
 
-        public void fillDic()
+        public static void fillDic()
         {
             Names.Add("Container", 0);
             Names.Add("Refrigerated", 0);
@@ -62,7 +121,7 @@ namespace APBD
             }
             else
             {
-                throw new Exception("Wrong load mass");
+                throw new OverFillException("Mass has been exceeded");
             }
         }
 
@@ -75,7 +134,6 @@ namespace APBD
                     return con;
                 }
             }
-
             return null;
         }
 
@@ -84,6 +142,110 @@ namespace APBD
             return
                 $"Container Serial Number: {sNumber}, Load Mass: {Mass}, Height: {Height}, Weight: {Weight}, Max mass: {MaxMass}, depth: {Depth}";
         }
+    }
+
+    public class LiquidContainer : Container, IHazardNotifier
+    {
+        private bool _DangerousContent;
+        public LiquidContainer(double height, double weight, double maxMass, double depth, bool dangerousContent, double mass) : base(height, weight, maxMass, depth, mass)
+        {
+            _DangerousContent = dangerousContent;
+            sNumber = $"KON-L-{Names["Liquid"]}";
+            Names["Liquid"]++;
+        }
+        
+        
+        public override void LoadMass(double massLoad)
+        {
+            if (_DangerousContent)
+            {
+                MaxMass = 0.5 * massLoad;
+            }
+                
+            if (massLoad >= 0 && massLoad <= MaxMass && !_DangerousContent)
+            {
+                MaxMass = 0.9 * massLoad;
+            }
+            else
+            {
+                throw new Exception("Tried to do a dangerous operation");
+            }
+        }
+        public void Notify()
+        {
+            Console.WriteLine($"HAZARD for container {sNumber}");
+        }
+    }
+
+    public class GasContainer : Container, IHazardNotifier
+    {
+        
+        public GasContainer(double height, double weight, double maxMass, double depth, double mass) : base(height, weight, maxMass, depth, mass)
+        {
+            sNumber = $"KON-G-{Names["Gas"]}";
+            Names["Gas"]++;
+            
+        }
+
+        public override void Empty()
+        {
+            Mass = 0.05 * Mass;
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine($"HAZARD for container {sNumber}");
+        }
+    }
+
+    public class RefrigeratedContainer : Container, IHazardNotifier
+    {
+        private string _Type;
+        private double _Temp;
+        private Dictionary<String, double> _Types = new Dictionary<string, double>();
+        public RefrigeratedContainer(double height, double weight, double maxMass, double depth, string type, double temp, double mass) : base(height, weight, maxMass, depth, mass)
+        {
+            AddTypes();
+            sNumber = $"KON-R-{Names["Refrigerated"]}";
+            Names["Refrigerated"]++;
+            _Temp = temp;
+            _Type = type;
+            if (_Temp > _Types[type])
+            {
+                Notify();
+            }
+        }
+        public void AddTypes()
+        {
+            _Types.Add("Bananas", 13.3);
+            _Types.Add("Apples", 4.4);
+            _Types.Add("Oranges", 7.2);
+            _Types.Add("Grapes", 1.7);
+            _Types.Add("Strawberries", 0.0);
+            _Types.Add("Carrots", 0.0);
+            _Types.Add("Lettuce", 1.7);
+            _Types.Add("Broccoli", 0.0);
+            _Types.Add("Tomatoes", 12.8);
+            _Types.Add("Milk", 4.4);
+            _Types.Add("Cheese", 2.2);
+            _Types.Add("Yogurt", 2.2);
+            _Types.Add("Eggs", 1.7);
+            _Types.Add("Chicken", -17.8);
+            _Types.Add("Fish", -17.8);
+            _Types.Add("Shrimp", -17.8);
+            _Types.Add("Ice Cream", -23.3);
+            _Types.Add("Frozen Vegetables", -17.8);
+            _Types.Add("Frozen Fruits", -23.3);
+            _Types.Add("Frozen Pizza", -17.8);
+        }
+        public void Notify()
+        {
+            Console.WriteLine($"HAZARD for container {sNumber}, temperature is too high!");
+        }
+    }
+    public interface IHazardNotifier
+    {
+        void Notify();
     }
     public class Ship
     {
@@ -94,8 +256,8 @@ namespace APBD
 
         public Ship(int maxAmountOfContainers,double maxSpeed, double maxMassOfContainers)
         {
-            maxMassOfContainers = MaxMassOfContainers;
-            maxAmountOfContainers = MaxAmountOfContainers;
+            MaxMassOfContainers = maxMassOfContainers;
+            MaxAmountOfContainers =maxAmountOfContainers;
             MaxSpeed = maxSpeed;
         }
 
@@ -113,7 +275,8 @@ namespace APBD
                 oldMass += con.getMass();
             }
             if (LoadedContainers.Count + toLoad.Count > MaxAmountOfContainers)
-            {
+            {   
+                Console.WriteLine(MaxAmountOfContainers);
                 throw new Exception("The Maximum Amount is exceeded");
             } 
             else if (totalMass + oldMass > MaxMassOfContainers * 1000)
@@ -161,4 +324,13 @@ namespace APBD
             return build.ToString();
         }
     }
+
+    public class OverFillException : Exception
+    {
+        public OverFillException(string msg): base (msg)
+        {
+            
+        }
+    }
+    
 }
